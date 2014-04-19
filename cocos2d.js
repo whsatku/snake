c0881c60cbc973390f2c59fdaa5bca21e7ae8dfc
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2014-04-19 13:05:26 +0700

    Cocos2D changed to single engine file

--- cocos2d.js
+++ cocos2d.js
@@ -8,8 +8,8 @@
 		frameRate: 60,
 		renderMode: 0,       //Choose of RenderMode: 0(default), 1(Canvas only), 2(WebGL only)
 		tag: 'gameCanvas', //the dom element to run cocos2d on
-		engineDir: 'cocos2d/',
-		//SingleEngineFile:'',
+		//engineDir: 'cocos2d/',
+		SingleEngineFile: 'cocos2d.min.js',
 		appFiles:[
 			'build/index.js',
 			'src/WebRTC.js',
